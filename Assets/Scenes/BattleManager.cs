a97c9c251f4c3c7448482769ea30db39dcc2ef19
Kapeta-16 <murija11162004@gmail.com>
2026-05-07 13:39:19 +0200

    added basic battle

--- Assets/Scenes/BattleManager.cs
+++ Assets/Scenes/BattleManager.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 
 public class BattleManager : MonoBehaviour
 {
+
     public int playerHP = 100;
     public int enemyHP = 80;
 
@@ -11,54 +12,80 @@ public class BattleManager : MonoBehaviour
     public TMP_Text playerHPText;
     public TMP_Text enemyHPText;
 
+    int maxPlayerHP;
+    int maxEnemyHP;
     bool playerTurn = true;
+    bool battleOver = false;
 
-    void Start() { UpdateUI(); }
+    void Start() { 
+        maxPlayerHP = playerHP;
+        maxEnemyHP = enemyHP;
+        statusText.text = "Ulazis u bitku";
+        UpdateUI(); 
+        }
 
     public void PlayerAttack()
     {
-        if (!playerTurn) return;
+        if (!playerTurn || battleOver) return;
         int dmg = Random.Range(10, 25);
         enemyHP -= dmg;
         statusText.text = "Napao si za " + dmg + " štete!";
         playerTurn = false;
-        CheckWin();
-        Invoke("EnemyTurn", 1.2f);
         UpdateUI();
+
+        if(CheckWin()) return;
+        Invoke("EnemyTurn", 1.2f);
     }
 
     public void PlayerHeal()
     {
-        if (!playerTurn) return;
+        if (!playerTurn || battleOver) return;
         int heal = Random.Range(10, 20);
-        playerHP = Mathf.Min(playerHP + heal, 100);
+        playerHP = Mathf.Min(playerHP + heal, maxPlayerHP);
         statusText.text = "Izliječio si " + heal + " HP!";
         playerTurn = false;
-        Invoke("EnemyTurn", 1.2f);
         UpdateUI();
+        Invoke("EnemyTurn", 1.2f);
     }
 
     void EnemyTurn()
     {
+        if(battleOver) return;
         int dmg = Random.Range(8, 18);
         playerHP -= dmg;
         statusText.text = "Neprijatelj napao za " + dmg + "!";
-        CheckWin();
         playerTurn = true;
         UpdateUI();
+        CheckWin();
     }
 
-    void CheckWin()
+    bool CheckWin()
     {
-        if (enemyHP <= 0) { statusText.text = "Pobijedio si!"; Invoke("GoBack", 2f); }
-        if (playerHP <= 0) { statusText.text = "Izgubio si..."; Invoke("GoBack", 2f); }
+        if (enemyHP <= 0) { 
+            statusText.text = "Pobijedio si!"; 
+            battleOver = true;
+            CancelInvoke();
+            Invoke("GoBack", 2f); 
+            return true;
+            }
+        if (playerHP <= 0) { 
+            statusText.text = "Izgubio si..."; 
+            battleOver = true;
+            CancelInvoke();
+            Invoke("GoBack", 2f); 
+            return true;
+            }
+            return false;
     }
 
-    void GoBack() { SceneManager.LoadScene("ExplorationScene"); }
+    void GoBack() { 
+        GameState.justFinishedBattle = true;
+        SceneManager.LoadScene("ExplorationScene");
+     }
 
     void UpdateUI()
     {
-        playerHPText.text = "Player HP: " + playerHP;
-        enemyHPText.text  = "Enemy HP: " + enemyHP;
+        playerHPText.text = "Player HP: " + playerHP + "/" + maxPlayerHP;
+        enemyHPText.text  = "Enemy HP: " + enemyHP + "/" + maxEnemyHP;
     }
 }
--- Assets/Scenes/EnemySpawner.cs
+++ Assets/Scenes/EnemySpawner.cs
@@ -2,10 +2,46 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+
+    public GameObject enemyPrefab;
+    public int minEnemies = 1;
+    public int maxEnemies = 5;
+
+    public Vector2 SpawnAreaMin = new Vector2(-8, -4);
+    public Vector2 SpawnAreaMax = new Vector2(8, 4);
+
+    //prikazano u inspectoru za lakse mjenjanje
+    public float firstSpawnDelay = 10f;
+    public float AfterBattleSpawnDelay = 3f;
+
     void Start()
     {
-        gameObject.SetActive(false);           // neprijatelj nevidljiv na početku
-        Invoke("ShowEnemy", 10f);              // pojavi se nakon 10 sekundi
+        //gameObject.SetActive(false);           // neprijatelj nevidljiv na početku
+        //Invoke("ShowEnemy", 10f);              // pojavi se nakon 10 sekundi
+
+        if(GameState.justFinishedBattle){ 
+            // vracanje is battle-a, spawnanje novih enemy-a
+            GameState.justFinishedBattle = false;
+            Invoke("SpawnNeprijatelja", AfterBattleSpawnDelay);
+        }else{ 
+            // fresh pocetak, cekanje 10 sekundi prije spawn-a
+            Invoke("SpawnNeprijatelja", firstSpawnDelay);
+        }
+    }
+
+    public void SpawnEnemies(){
+        int count = Random.Range(minEnemies, maxEnemies + 1);
+
+        for(int i = 0; i < count; i++){
+            //coordinate za spawn enemy-a
+            float x = Random.Range(SpawnAreaMin.x, SpawnAreaMax.x);
+            float y = Random.Range(SpawnAreaMin.y, SpawnAreaMax.y);
+
+            Vector3 spawnPosition = new Vector3(x, y, 0);
+
+            Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+        }
+        Debug.Log("Spawned " + count + " enemies");
     }
 
     void ShowEnemy()

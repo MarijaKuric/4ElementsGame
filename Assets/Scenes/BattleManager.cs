f5297daaa7f6cf898068c0a367d6784479fe70aa
elenh4 <elenhaj77@gmail.com>
2026-05-12 12:41:14 +0200

    Update BattleManager.cs

--- Assets/Scenes/BattleManager.cs
+++ Assets/Scenes/BattleManager.cs
@@ -27,7 +27,6 @@ public class BattleManager : MonoBehaviour
     bool playerTurn = true;
     bool battleOver = false;
 
-    int attackCounter = 0;
     Animator fireAnimator;
     Animator explosionAnimator;
     Animator doorsOpen;
@@ -52,6 +51,7 @@ public class BattleManager : MonoBehaviour
     public void PlayerAttack()
     {
         if (!playerTurn || battleOver) return;
+        int attackCounter = 0;
         int dmg = Random.Range(10, 25);
         enemyHP = Mathf.Max(enemyHP - dmg, 0);
         string attackMessage = "";

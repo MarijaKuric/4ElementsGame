2fde252ae27126af23be1413a14072af84b9b42c
elenh4 <elenhaj77@gmail.com>
2026-07-10 12:57:13 +0200

    Dodani pasivni ability-ti

--- Assets/Skripte/AbilityData.cs
+++ Assets/Skripte/AbilityData.cs
@@ -3,6 +3,19 @@ using UnityEngine;
 public enum AbilityType { Basic, Special, Passive }
 public enum Element { Neutral, Fire, Water, Wind }
 
+public enum PassiveEffect
+{
+    None,
+    DamageReduction,
+    EnergyRegen,
+    BurnBonusDamage,
+    CritHeal,
+    RegenHeal,
+    BlockChance,
+    DodgeChance,
+    FirstStrikeBonus
+}
+
 [CreateAssetMenu(fileName = "NewAbility", menuName = "4Elements/Ability")]
 public class AbilityData : ScriptableObject
 {
@@ -15,4 +28,10 @@ public class AbilityData : ScriptableObject
     public int baseDamage;
     public int damagePerLevel;
     public Sprite icon;
+
+    [Header("Passive Only")]
+    public PassiveEffect passiveEffect = PassiveEffect.None;
+    [Range(0f, 1f)] public float passiveChance = 0f;
+    public int passiveValue = 0;
+    public int passiveValuePerLevel = 0;
 }

37f7271e30c3ea3f6e5d8922482cc5775ab27c6e
aluksa1 <aluksa1@student.unipu.hr>
2026-07-10 17:28:39 +0200

    add left side character  animation, add animation on all levels

--- Assets/Skripte/PlayerMovement.cs
+++ Assets/Skripte/PlayerMovement.cs
@@ -5,19 +5,26 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5f;
     private Rigidbody2D rb;
     private Animator animator;
+    private SpriteRenderer sr;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     void FixedUpdate()
     {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
-        rb.linearVelocity = new Vector2(x, y) * speed;
 
+        rb.linearVelocity = new Vector2(x, y) * speed;
         animator.SetBool("isWalking", x != 0 || y != 0);
+
+        if (x < 0)
+            sr.flipX = true;   // moving left
+        else if (x > 0)
+            sr.flipX = false;  // moving right
     }
 }
